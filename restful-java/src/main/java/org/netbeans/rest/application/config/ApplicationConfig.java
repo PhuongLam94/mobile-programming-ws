501332cfc13906d9f28bb10ff810ab66e29d3672
phuonglam94 <51202846@hcmut.edu.vn>
2016-02-27 20:23:15 +0700

    them filter

--- restful-java/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ restful-java/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -5,7 +5,7 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import vn.khmt.restful.UserService;
-
+import vn.khmt.restful.COSRFilter;
 /**
  *
  * @author The Nhan
@@ -17,6 +17,7 @@ public class ApplicationConfig extends Application {
     public Set<Class<?>> getClasses() {
         Set<Class<?>> s = new HashSet<>();
         s.add(UserService.class);
+        s.add(COSRFilter.class);
         return s;
     }
 }
